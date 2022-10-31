9af29c1e5d4d393472fbdd0c3e30362b3293ca53
Rudyboy <rudylepretre@gmail.com>
2022-10-31 17:27:13 +0100

    Add Navigation drawer

--- app/src/main/java/com/rudy/go4lunch/ui/MainActivity.java
+++ app/src/main/java/com/rudy/go4lunch/ui/MainActivity.java
@@ -1,36 +1,59 @@
 package com.rudy.go4lunch.ui;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 import com.rudy.go4lunch.R;
 import com.rudy.go4lunch.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     ActivityMainBinding binding;
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTheme(R.style.AppTheme);
         initUi();
         setUpLogin();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (this.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            this.drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     private void initUi() {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+        initToolbar();
         initBottomNavigationView();
+        initDrawerLayout();
+        initMenuNavigationView();
     }
 
     private void initBottomNavigationView() {
@@ -45,13 +68,48 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-//        MenuInflater inflater = getMenuInflater();
-//        inflater.inflate(R.menu.menu, menu);
-//        return true;
-        return super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
     }
 
     private void setUpLogin() {
         WelcomeScreen.navigate(MainActivity.this);
+    }
+
+    private void initToolbar() {
+        this.toolbar = binding.toolbar;
+        setSupportActionBar(toolbar);
+    }
+
+    private void initDrawerLayout() {
+        this.drawerLayout = binding.mainDrawerLayout;
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    private void initMenuNavigationView() {
+        this.navigationView = binding.navigationView;
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.activity_main_drawer_lunch:
+                break;
+            case R.id.activity_main_drawer_settings:
+                break;
+            case R.id.activity_main_drawer_logout:
+                break;
+            default:
+                break;
+        }
+        this.drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
